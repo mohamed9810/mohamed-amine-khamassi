06fd1203e22858b5261573479be5b4e25a906b3e
mohamed9810 <159959830+mohamed9810@users.noreply.github.com>
2024-02-13 22:36:17 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+/*EX1
+function max2(a,b){
+    if(a>b)
+    return  a
+else
+    return b;
+}
+
+
+
+console.log(max2(5,8))
+console.log(max2(15,8))
+console.log(max2(18,18))*/
+
+/*EX2
+function aff(char){
+    return char
+}
+
+console.log(aff("bonjour"))*/
+
+/*EX3
+function somme(a,b){
+    a=prompt("Donner le premier nombre")
+    b=prompt("Donner la deuxieme nombre")
+    alert(Number(a)+Number(b))
+}
+
+console.log(somme(3,7))*/
+
+
+
+
+
+/* EX4 
+const container = document.getElementById("a")
+
+function into(){
+    container.innerHTML = "Enter"
+}
+
+function out(){
+    container.innerHTML = "Out"
+}
+
+function bg(){
+    container.style.backgroundColor = "red"
+} */
